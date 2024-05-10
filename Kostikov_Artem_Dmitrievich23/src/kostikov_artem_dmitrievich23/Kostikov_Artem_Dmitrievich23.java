6ace7e1e900b4a30ab536b3a585b00a7862c19cf
DegroDuffy <duffy.3tm@gmail.com>
2024-05-10 18:21:40 +0300

    Add files via upload

---
+++ Kostikov_Artem_Dmitrievich23/src/kostikov_artem_dmitrievich23/Kostikov_Artem_Dmitrievich23.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kostikov_artem_dmitrievich23;
+
+/**
+ *
+ * @author shest
+ */
+public class Kostikov_Artem_Dmitrievich23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
